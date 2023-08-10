d0cf3db76c9a0ae346b5d443dae91ef7590be3f5
Benjamin Morris <benjamin.morris@prd-aics-dcp-036.corp.alleninstitute.org>
2023-08-10 12:20:47 -0700

    remove non-object steps

--- morflowgenesis/steps/load_df.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from prefect import task
-
-@task
-def load_df(path):
-    return pd.read_csv(path)
--- morflowgenesis/steps/upload.py
+++
@@ -1,6 +0,0 @@
-from prefect import task
-
-@task
-def upload_img(img):
-    print("Uploading image...", img)
-    return img
