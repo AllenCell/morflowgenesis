8882070a46303fe05ce3e14a4e29925b1945736d
Benjamin Morris <benjamin.morris@prd-aics-dcp-036.corp.alleninstitute.org>
2023-08-15 15:36:16 -0700

    split out workflow loading, eliminate unnecessary flows

--- morflowgenesis/bin/run_workflow.py
+++ morflowgenesis/bin/run_workflow.py
@@ -1,43 +1,23 @@
 import hydra
 from omegaconf import DictConfig
-from prefect import flow, get_run_logger, task
-from prefect.task_runners import ConcurrentTaskRunner, SequentialTaskRunner
 from morflowgenesis.bin.run_step import run_step
 from pathlib import Path
-import pickle
-import OmegaConf
+from omegaconf import OmegaConf
+from morflowgenesis.steps.load_workflow_state import get_workflow_state
 
-@task
-def load_image_objects(obj_path):
-    with open(obj_path, 'rb') as f:
-        return pickle.load(f)
-    
-@flow(task_runner=ConcurrentTaskRunner())
-def get_workflow_state(cfg):
-    """
-    Load image objects as previous workflow state
-    """
-    logger= get_run_logger()
-    existing_objects= []
-    img_object_dir =Path(cfg['working_dir'])/'_ImageObjectStore'
-    for obj_path in img_object_dir.glob('*.pkl'):
-        existing_objects.append(load_image_objects.submit(obj_path))
-    logger.info(f'Loaded {len(existing_objects)} image objects')
-    return [x.result() for x in existing_objects]
-
-
-def save_workflow_config( cfg):
-    working_dir = Path(cfg['working_dir'])
+def save_workflow_config(working_dir, cfg):
     with open(working_dir/'workflow_config.yaml', 'w') as f:
         OmegaConf.save(config= cfg, f = f)
 
-@flow(task_runner=SequentialTaskRunner())
 def WorkflowRunner(cfg):
     """
     Sequentially run config-specified steps, starting with the previous workflow state
     and passing output from step n-1 as input to step n
     """
-    save_workflow_config(cfg)
+    working_dir = Path(cfg['working_dir'])
+    working_dir.mkdir(exist_ok=True, parents=True)
+    save_workflow_config(working_dir, cfg)
+
     prev_output = get_workflow_state(cfg)
     for step_name, step_meta in cfg['steps'].items():
         step_fn = step_meta['function']
---
+++ morflowgenesis/steps/load_workflow_state.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from prefect import flow, get_run_logger, task
+from prefect.task_runners import ConcurrentTaskRunner
+import pickle
+
+@task
+def load_image_objects(obj_path):
+    with open(obj_path, 'rb') as f:
+        return pickle.load(f)
+    
+@flow(task_runner=ConcurrentTaskRunner())
+def get_workflow_state(cfg):
+    """
+    Load image objects as previous workflow state
+    """
+    logger= get_run_logger()
+    existing_objects= []
+    img_object_dir =Path(cfg['working_dir'])/'_ImageObjectStore'
+    for obj_path in img_object_dir.glob('*.pkl'):
+        existing_objects.append(load_image_objects.submit(obj_path))
+    logger.info(f'Loaded {len(existing_objects)} image objects')
+    return [x.result() for x in existing_objects]
