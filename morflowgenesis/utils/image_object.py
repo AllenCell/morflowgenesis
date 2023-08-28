a00aa4d114832dde88e64efb7cafe5557ee5c22e
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-08-28 16:36:52 -0700

    make step public

--- morflowgenesis/utils/image_object.py
+++ morflowgenesis/utils/image_object.py
@@ -13,14 +13,14 @@ class ImageObject(BaseModel):
     metadata: Optional[Dict] = None
     save_path: Path = Path("")
     id: str = ""
-    _steps: Dict[str, StepOutput] = {}
+    steps: Dict[str, StepOutput] = {}
 
     def __init__(
         self,
         working_dir,
         source_path,
         metadata=None,
-        _steps={},
+        steps={},
         save_path: Path = Path(""),
         id: str = "",
     ):
@@ -28,7 +28,7 @@ class ImageObject(BaseModel):
             working_dir=Path(working_dir),
             source_path=source_path,
             metadata=metadata,
-            _steps=_steps,
+            steps=steps,
         )
         self.id = hashlib.sha224(bytes(source_path + str(metadata), "utf-8")).hexdigest()
         self.save_path = Path(self.working_dir / "_ImageObjectStore" / f"{self.id}.json")
@@ -36,16 +36,16 @@ class ImageObject(BaseModel):
 
     def step_is_run(self, step_name):
         # for a step to be considered run, must be in history and output must exist
-        return step_name in self._steps and self.get_step(step_name).path.exists()
+        return step_name in self.steps and self.get_step(step_name).path.exists()
 
     def add_step_output(self, output):
         # add output to image object
         step_name = f"{output.step_name}_{output.output_name}"
-        self._steps[step_name] = output
+        self.steps[step_name] = output
 
     def get_step(self, step_name):
         # load StepOutput object
-        return self._steps[step_name]
+        return self.steps[step_name]
 
     def load_step(self, step_name):
         # load output from StepOutput object
