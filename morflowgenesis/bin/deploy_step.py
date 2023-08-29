be5852b18c024c1df16e3b3838d85e12d76392ca
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-08-29 12:13:19 -0700

    simplify update syntax

--- morflowgenesis/bin/deploy_step.py
+++ morflowgenesis/bin/deploy_step.py
@@ -1,24 +1,27 @@
-from copy import deepcopy
-
 from hydra._internal.utils import _locate
-
 from morflowgenesis.utils import BlockDeployment, encode_dict_to_json_base64
 
 
-def _merge_configs(cfg, step_cfg, key):
-    new = deepcopy(cfg[key])
-    new.update(step_cfg.get(key, {}))
-    return new
+def merge_dicts(dict1, dict2):
+    for key, value in dict2.items():
+        if isinstance(value, dict) and key in dict1 and isinstance(dict1[key], dict):
+            merge_dicts(dict1[key], value)
+        else:
+            dict1[key] = value
+    return dict1
+
 
 def deploy_step(cfg, step_cfg):
     step_fn = _locate(step_cfg["function"])
     # path to code within repo
     *entrypoint, flow_name = step_cfg["function"].split(".")
     entrypoint = "/".join(entrypoint) + f".py:{flow_name}"
+    infra_overrides = merge_dicts(cfg['infra_overrides'], step_cfg.get('infra_overrides', {}))
 
-    infra_overrides = _merge_configs(cfg, step_cfg, "infra_overrides")
-    if "dask_cluster" in step_cfg:
-        encoded_dask_cluster = encode_dict_to_json_base64(step_cfg["dask_cluster"])
+    # we treate cpu steps as the default - null must be passed for steps that don't want to use GPU cluster
+    if step_cfg.get("dask_cluster", True) is not None:
+        merged_cluster_args= merge_dicts(cfg['dask_cluster'], step_cfg.get('dask_cluster', {}))
+        encoded_dask_cluster = encode_dict_to_json_base64(merged_cluster_args)
         if "env" not in infra_overrides:
             infra_overrides["env"] = {}
         infra_overrides["env"]["DASK_CLUSTER"] = encoded_dask_cluster
--- morflowgenesis/bin/run_workflow.py
+++ morflowgenesis/bin/run_workflow.py
@@ -1,76 +1,66 @@
-import os
+import asyncio
+from pathlib import Path
+from copy import deepcopy
 
-import numpy as np
-import pandas as pd
-from prefect import flow, task
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from prefect import flow
 
-from morflowgenesis.utils import create_task_runner
-from morflowgenesis.utils.step_output import StepOutput
-from morflowgenesis.utils.image_object import ImageObject
+from prefect.deployments import run_deployment
+from prefect.task_runners import SequentialTaskRunner
 
-
-
-def get_start_and_end_pts(roi):
-    # from roi string like [0, 5, 2, 30, 15, 18] defining a z 0:5, y 2:30, x 15:18
-    # return [0, 2, 15], [5, 30, 18]
-    return np.asarray(roi)[::2], np.asarray(roi)[1::2]
+from morflowgenesis.bin.deploy_step import deploy_step
+from morflowgenesis.bin.run_step import run_step
+from morflowgenesis.utils import BlockDeployment
 
 
-def str_to_array(roi):
-    # roi string to numpy array
-    return np.asarray(roi.split(",")).astype(int)
+def save_workflow_config(working_dir, cfg):
+    with open(Path(working_dir) / "workflow_config.yaml", "w") as f:
+        OmegaConf.save(config=cfg, f=f)
 
 
-@task
-def iou_from_roi(roi1: str, roi2: str, eps: float = 1e-8):
-    # calculate iou between roi strings from single cell dataset csv
-    roi1_starts, roi1_ends = get_start_and_end_pts(roi1)
-    roi2_starts, roi2_ends = get_start_and_end_pts(roi2)
+@flow(log_prints=True, task_runner=SequentialTaskRunner())
+async def morflowgenesis(cfg):
+    """Sequentially run config-specified steps, starting with the previous workflow state and
+    passing output from step n-1 as input to step n."""
+    working_dir = Path(cfg["working_dir"])
+    working_dir.mkdir(exist_ok=True, parents=True)
+    save_workflow_config(working_dir, cfg)
 
-    max_start = np.max([roi1_starts, roi2_starts], axis=0)
-    min_end = np.min([roi1_ends, roi2_ends], axis=0)
-    intersection = np.prod(min_end - max_start)
-
-    min_start = np.min([roi1_starts, roi2_starts], axis=0)
-    max_end = np.max([roi1_ends, roi2_ends], axis=0)
-    union = np.prod(max_end - min_start)
-    return (intersection + eps) / (union + eps)
+    for step_cfg in cfg["steps"]:
+        await run_step(step_cfg, working_dir/ "_ImageObjectStore")
 
 
-@flow(task_runner=create_task_runner(), log_prints=True)
-def match_cells(image_object_path, step_name, output_name, pred_step, label_step, iou_thresh=0.9):
-    image_object = ImageObject.parse_file(image_object_path)
-    # check if step already run
-    if image_object.step_is_run(f"{step_name}_{output_name}"):
-        print(f"Skipping step {step_name}_{output_name} for image {image_object.id}")
-        return image_object
+# default config is morflowgenesis/configs/workflow/config.yaml
+@hydra.main(version_base="1.3", config_path="../configs/workflow", config_name="config.yaml")
+def main(_cfg: DictConfig):
+    cfg = OmegaConf.to_container(_cfg, resolve=True)
+    deployment_name = cfg.get("deployment_name", "default")
 
-    pred_match_to_label = {"pred_cellid": [], "label_cellid": [], "iou": []}
-    pred_df = image_object.load_step(pred_step)
-    label_df = image_object.load_step(label_step)
-    # match each pred cell to best ground truth cell where match exceeds iou_thresh
-    for pred_row in pred_df.itertuples():
-        best_iou = -np.inf
-        best_cellid = None
-        for label_row in label_df.itertuples():
-            iou = iou_from_roi(str_to_array(pred_row.roi), str_to_array(label_row.roi))
-            # update best iou
-            if iou > best_iou:
-                best_iou = iou
-                best_cellid = label_row.CellId
-        # record a match if best iou exceeds threshold
-        if best_iou > iou_thresh:
-            pred_match_to_label["pred_cellid"].append(pred_row.CellId)
-            pred_match_to_label["label_cellid"].append(best_cellid)
-            pred_match_to_label["iou"].append(best_iou)
+    debug = cfg.get("debug", False)
 
-    step_output = StepOutput(
-        image_object.working_dir,
-        step_name,
-        output_name,
-        output_type="csv",
-        image_id=image_object.id,
-    )
-    step_output.save(pd.DataFrame(pred_match_to_label))
-    image_object.add_step_output(step_output)
-    image_object.save()
+    if cfg.get("deploy", False):
+        if not debug:
+            dep = BlockDeployment.build_from_flow(
+                morflowgenesis,
+                deployment_name,
+                apply=False,
+                path=cfg["path"],
+                entrypoint=cfg["entrypoint"],
+                infra_overrides=cfg["infra_overrides"],
+            )
+            dep.apply(cfg["pull"])
+
+        for step_cfg in cfg["steps"]:
+            deploy_step(deepcopy(cfg), step_cfg)
+
+    if not debug:
+        run_deployment(name=f"morflowgenesis/{deployment_name}", parameters=cfg)
+    else:
+        # run superworkflow locally, better error readouts
+        asyncio.run(morflowgenesis(cfg))
+
+
+if __name__ == "__main__":
+    # asyncio.run(main())
+    main()
