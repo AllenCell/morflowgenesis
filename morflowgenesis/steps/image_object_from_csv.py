e5e9512d9c3a1ed3a1c03e089975a7c107675a07
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-08-29 16:29:50 -0700

    remove image_objects args

--- morflowgenesis/steps/image_object_from_csv.py
+++ morflowgenesis/steps/image_object_from_csv.py
@@ -4,8 +4,9 @@ import pandas as pd
 from aicsimageio import AICSImage
 from prefect import flow, task
 
-from morflowgenesis.utils import create_task_runner
-from morflowgenesis.utils.image_object import ImageObject, StepOutput
+from prefect.task_runners import SequentialTaskRunner
+from morflowgenesis.utils.image_object import ImageObject
+from morflowgenesis.utils.step_output import StepOutput
 
 
 @task
@@ -26,9 +27,8 @@ def generate_object(
         obj.add_step_output(step_output)
     obj.save()
 
-@flow(task_runner=create_task_runner(), log_prints=True)
+@flow(task_runner=SequentialTaskRunner(), log_prints=True)
 def generate_objects(
-    image_objects,
     working_dir,
     step_name,
     csv_path,
@@ -36,6 +36,8 @@ def generate_objects(
     non_source_columns,
     metadata_column=None,
 ):
+    image_objects = [ImageObject.parse_file(obj_path) for obj_path in (working_dir/ "_ImageObjectStore").glob('*.json')]
+
     """Generate a new image object for each row in the csv file."""
     df = pd.read_csv(csv_path)
 
