44003710b5a72f9a793f36b59c5786c43c72dc94
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-08-30 12:35:51 -0700

    raise error on failure

--- morflowgenesis/bin/run_step.py
+++ morflowgenesis/bin/run_step.py
@@ -2,6 +2,7 @@ import asyncio
 
 from prefect.deployments import run_deployment
 from slugify import slugify
+from prefect.states import Completed
 
 
 async def run_step(step_cfg, prev_output):
@@ -9,17 +10,18 @@ async def run_step(step_cfg, prev_output):
     step_type = step_cfg.get("step_type", "list")
     step_args = step_cfg["args"]
     step_args["step_name"] = step_fn.split(".")[-1]
-
     flow_name = slugify(step_args["step_name"])
     deployment_name = slugify(step_cfg.get("deployment_name", "default"))
     full_deployment_name = f"{flow_name}/{deployment_name}"
     if step_type == "gather":
-        await run_deployment(
+        out = await run_deployment(
             full_deployment_name,
             parameters=step_args,
         )
+        if not out.state.type !=  Completed:
+            raise RuntimeError(f"Step {step_fn} completed with state {out.state_name}")
     else:
-        await asyncio.gather(
+        out = await asyncio.gather(
             *[run_deployment(
                     full_deployment_name, 
                     parameters = {"image_object_path": object_path, **step_args}
@@ -27,3 +29,7 @@ async def run_step(step_cfg, prev_output):
                 for object_path in prev_output.glob('*.json')
             ]
         )
+        for result in out:
+            if not result.state.type !=  Completed:
+                raise RuntimeError(f"Step {step_fn} completed with state {result.state_name}")
+
