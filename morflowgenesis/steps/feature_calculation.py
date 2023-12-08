b505d9d0dc639af82bcf0ba4c0c8aa4795ea6bc0
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2023-12-08 15:14:36 -0800

    add fov-level features

--- morflowgenesis/steps/feature_calculation.py
+++ morflowgenesis/steps/feature_calculation.py
@@ -4,10 +4,13 @@ from typing import List, Optional, Union
 import numpy as np
 import pandas as pd
 import tqdm
+import torch
 from aicsimageio import AICSImage
 from aicsshparam import shparam, shtools
 from prefect import flow, task
 from skimage.measure import label
+
+from torchmetrics.classification import BinaryF1Score
 
 from morflowgenesis.utils import ImageObject, StepOutput, create_task_runner, submit
 
@@ -51,6 +54,18 @@ def get_largest_cc(im):
     largest_cc = np.argmax(np.bincount(im.flatten())[1:]) + 1
     return im == largest_cc
 
+def get_f1(img, reference):
+    return {'f1':BinaryF1Score()(torch.from_numpy(img), torch.from_numpy(reference).item())}
+
+FEATURE_EXTRACTION_FUNCTIONS = {
+    "volume": get_volume,
+    "height": get_height,
+    "shcoeff": get_shcoeff,
+    "n_pieces": get_n_pieces,
+    'f1': get_f1,
+}
+
+
 
 def compute_nucleolus_feats(img):
     nucleolus = img[0]
@@ -71,14 +86,6 @@ def compute_nucleolus_feats(img):
     }
 
 
-FEATURE_EXTRACTION_FUNCTIONS = {
-    "volume": get_volume,
-    "height": get_height,
-    "shcoeff": get_shcoeff,
-    "n_pieces": get_n_pieces,
-}
-
-
 def append_dict(features_dict, new_dict):
     for k, v in new_dict.items():
         try:
@@ -87,9 +94,54 @@ def append_dict(features_dict, new_dict):
             features_dict[k] = [v]
     return features_dict
 
+def ensure_channel_first(img):
+    if len(img.shape) == 3:
+        img = np.expand_dims(img, 0)
+    return img
+
 
 @task
-def get_features(row, features, channels):
+def get_roi_features(img, features, channels):
+    img = ensure_channel_first(img)
+    channels = channels or range(img.shape[0])
+    features_dict = {}
+    for ch in channels:
+        ch_img = img[ch]
+        if np.all(ch_img == 0):
+            continue
+        for feat in features:
+            if feat not in FEATURE_EXTRACTION_FUNCTIONS:
+                print(
+                    f"Feature {feat} not found. Options are {FEATURE_EXTRACTION_FUNCTIONS.keys()}"
+                )
+                continue
+            feats = FEATURE_EXTRACTION_FUNCTIONS[feat](ch_img)
+            append_dict(features_dict, feats)
+    return pd.DataFrame(features_dict)
+
+@task
+def get_matched_roi_features(img, features, channels, reference):
+    img = ensure_channel_first(img)
+    reference = ensure_channel_first(reference)
+    channels = channels or range(img.shape[0])
+    features_dict = {}
+    for ch in channels:
+        ch_img = img[ch]
+        if np.all(ch_img == 0):
+            continue
+        for feat in features:
+            if feat not in FEATURE_EXTRACTION_FUNCTIONS:
+                print(
+                    f"Feature {feat} not found. Options are {FEATURE_EXTRACTION_FUNCTIONS.keys()}"
+                )
+                continue
+            feats = FEATURE_EXTRACTION_FUNCTIONS[feat](ch_img, reference[ch])
+            append_dict(features_dict, feats)
+    return pd.DataFrame(features_dict)
+
+
+@task
+def get_cell_features(row, features, channels):
     features_dict = {}
     multi_index = []
 
@@ -120,7 +172,7 @@ def get_features(row, features, channels):
 
 
 @task()
-def get_matched_features(row, features, channels, reference_channel):
+def get_matched_cell_features(row, features, channels, reference_channel):
     features_dict = {}
     img = AICSImage(row["crop_seg_path"])
     channel_names = img.channel_names
@@ -172,6 +224,7 @@ def run_object(
     reference_channel,
     features,
     channels,
+    reference_step,
     run_within_object,
 ):
     """General purpose function to run a task across an image object.
@@ -180,32 +233,40 @@ def run_object(
     list of futures and the output object. Otherwise, run the task as a function and return the
     results and an output object
     """
-    cell_df = image_object.load_step(input_step)
-    results = []
-    for row in tqdm.tqdm(cell_df.itertuples()):
-        row = row._asdict()
-        if reference_channel is None:
-            results.append(
-                submit(
-                    get_features,
-                    as_task=run_within_object,
-                    row=row,
-                    features=features,
-                    channels=channels,
+    input = image_object.load_step(input_step)
+    # single cell calculations
+    if isinstance(input, pd.DataFrame):
+        results = []
+        for row in tqdm.tqdm(input.itertuples()):
+            row = row._asdict()
+            if reference_channel is None:
+                results.append(
+                    submit(
+                        get_cell_features,
+                        as_task=run_within_object,
+                        row=row,
+                        features=features,
+                        channels=channels,
+                    )
                 )
-            )
+            else:
+                results.append(
+                    submit(
+                        get_matched_cell_features,
+                        as_task=run_within_object,
+                        row=row,
+                        features=features,
+                        channels=channels,
+                        reference_channel=reference_channel,
+                    )
+                )
+    # fov calculations
+    elif isinstance(input, np.ndarray):
+        if reference_step is None:
+            results = [submit(get_roi_features, as_task=run_within_object, img=input, features=features, channels=channels)]
         else:
-            results.append(
-                submit(
-                    get_matched_features,
-                    as_task=run_within_object,
-                    row=row,
-                    features=features,
-                    channels=channels,
-                    reference_channel=reference_channel,
-                )
-            )
-
+            reference = image_object.load_step(reference_step)
+            results= [submit(get_matched_roi_features, as_task = run_within_object, img=input, features=features, channels=channels, reference = reference)]
     output = StepOutput(
         image_object.working_dir,
         "calculate_features",
@@ -225,6 +286,7 @@ def calculate_features(
     features: List[str],
     reference_channel: Optional[str] = None,
     channels: Optional[List[str]] = None,
+    reference_step: Optional[str] = None,
 ):
     """
     Parameters
@@ -234,13 +296,15 @@ def calculate_features(
     output_name: str
         Name of the output
     input_step: str
-        Name of the input step (usually a single cell dataset step)
+        Name of the input step (a single cell dataset or segmentation step)
     features: List[str]
         List of names of features to calculate
     reference_channel: Optional[str]
         Name of the reference channel to use for spherical harmonics alignment
     channels: Optional[List[str]]
         List of channel names to use for feature calculation. If None, use all channels
+    reference_step: Optional[str]
+        For FOV-based features, another image can be used to calculate FOV similarity features (like F1, Dice, etc.)
     """
     # if only one image is passed, run across objects within that image. Otherwise, run across images
     image_objects = [ImageObject.parse_file(path) for path in image_object_paths]
@@ -258,6 +322,7 @@ def calculate_features(
                 reference_channel=reference_channel,
                 features=features,
                 channels=channels,
+                reference_step=reference_step,
                 run_within_object=run_within_object,
             )
         )
