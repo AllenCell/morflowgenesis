045bfe9864993a5ba285fe6fd1318e8ecbe051e7
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-01-23 15:02:38 -0800

    keep segmentation labels

--- morflowgenesis/steps/alignment.py
+++ morflowgenesis/steps/alignment.py
@@ -1,39 +1,39 @@
-from prefect import task, flow
 import numpy as np
-from morflowgenesis.utils import (
-    ImageObject,
-    StepOutput,
-    create_task_runner,
-    submit,
-)
+from prefect import flow, task
 from skimage.segmentation import find_boundaries
+
+from morflowgenesis.utils import ImageObject, StepOutput, create_task_runner, submit
 
 
 @task
 def align(
-    image_object, image_step, segmentation_step, boundary=False,
+    image_object,
+    image_step,
+    segmentation_step,
+    boundary=False,
 ):
     img = image_object.load_step(image_step)
-    seg = image_object.load_step(segmentation_step)>0
+    seg = image_object.load_step(segmentation_step)
+
     assert len(img.shape) == len(seg.shape) == 3, "Image and segmentation must be 3D"
     if boundary:
         seg = find_boundaries(seg)
     # crop to z with segmentation
-    z, _, _ = np.where(seg)
-    mask = seg[z.min():z.max()]
+    z, _, _ = np.where(seg > 0)
+    mask = seg[z.min() : z.max()]
 
     # ensure same xy size
-    minimum_shape= np.minimum(img.shape[-2:], mask.shape[-2:])
-    img = img[:, :minimum_shape[0], :minimum_shape[1]]
-    mask = mask[:, :minimum_shape[0], :minimum_shape[1]]
+    minimum_shape = np.minimum(img.shape[-2:], mask.shape[-2:])
+    img = img[:, : minimum_shape[0], : minimum_shape[1]]
+    mask = mask[:, : minimum_shape[0], : minimum_shape[1]]
 
     # sliding window to find z where segmentation covers most signal
     out = []
-    for i in range(0, img.shape[0]-mask.shape[0]):
-        out.append(np.sum(img[i:i+mask.shape[0]] * mask))
+    for i in range(0, img.shape[0] - mask.shape[0]):
+        out.append(np.sum(img[i : i + mask.shape[0]] * (mask > 0)))
     best_start_z = np.argmax(out)
-    new_seg = np.stack([np.zeros(mask.shape[-2:])]*img.shape[0])
-    new_seg[best_start_z:best_start_z+mask.shape[0]] = seg[z.min():z.max()]
+    new_seg = np.stack([np.zeros(mask.shape[-2:])] * img.shape[0])
+    new_seg[best_start_z : best_start_z + mask.shape[0]] = seg[z.min() : z.max()]
 
     # add result to image object
     output = StepOutput(
@@ -46,9 +46,14 @@ def align(
     output.save(new_seg)
     return output
 
+
 @task
 def run_object(
-    image_object, image_step, segmentation_steps, run_within_object, boundary=False,
+    image_object,
+    image_step,
+    segmentation_steps,
+    run_within_object,
+    boundary=False,
 ):
     results = []
     for step in segmentation_steps:
@@ -64,12 +69,10 @@ def run_object(
         )
     return image_object, results
 
+
 @flow(task_runner=create_task_runner(), log_prints=True)
 def align_segmentations_to_image(
-    image_object_paths,
-    image_step,
-    segmentation_steps,
-    boundary=False
+    image_object_paths, image_step, segmentation_steps, boundary=False
 ):
     image_objects = [ImageObject.parse_file(path) for path in image_object_paths]
     run_within_object = len(image_objects) == 1
@@ -98,4 +101,4 @@ def align_segmentations_to_image(
             object, results = results.result()
         for result in results:
             object.add_step_output(result)
-
+        object.save()
