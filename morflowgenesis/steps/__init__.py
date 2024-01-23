4604bded5880711e6d3da74ffdef3b1826e2b1eb
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-01-23 15:10:25 -0800

    update step names

--- morflowgenesis/steps/__init__.py
+++ morflowgenesis/steps/__init__.py
@@ -1,6 +1,9 @@
 from .contact_sheet import segmentation_contact_sheet
+from .create_manifest_nucmorph import create_manifest
 from .feature_calculation import calculate_features
-from .generate_shape_space import make_shape_space
+from .generate_thresholds import threshold
+
+# from .generate_shape_space import make_shape_space
 from .image_object_from_csv import generate_objects
 from .pca import run_pca
 from .plot import run_plot
@@ -13,4 +16,4 @@ from .segmentation_seeded_watershed import run_watershed
 from .separate_instances import generate_mask
 from .single_cell_dataset import single_cell_dataset
 from .split_image import split_image
-from .tracking import run_tracking
+from .tracking import tracking
