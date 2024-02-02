25f8c655fcbfb54b9ad2cbc43d99dbdafb020bb7
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2024-02-02 15:58:37 -0800

    add thresholding

--- morflowgenesis/steps/generate_thresholds.py
+++ morflowgenesis/steps/generate_thresholds.py
@@ -2,15 +2,16 @@ import numpy as np
 from prefect import flow, task
 from skimage.measure import label as run_label
 
-from morflowgenesis.utils import ImageObject, StepOutput, create_task_runner, submit
+from morflowgenesis.utils import ImageObject, StepOutput, submit, parallelize_across_images
 
 
 @task
-def run_threshold(image_object, img, output_name, thresh, label):
+def run_threshold(image_object, input_step, output_name, thresh, label):
+    img = image_object.load_step(input_step)
     step = StepOutput(
         working_dir=image_object.working_dir,
         step_name="threshold",
-        output_name=f"{output_name}_{thresh}",
+        output_name=f"{output_name}/{thresh}",
         output_type="image",
         image_id=image_object.id,
     )
@@ -21,36 +22,10 @@ def run_threshold(image_object, img, output_name, thresh, label):
     return step
 
 
-@task(name="threshold")
-def run_object(image_object, output_name, input_step, threshold_range, run_within_object, label):
-    """General purpose function to run a task across an image object.
-
-    If run_within_object is True, parallelize across thresholds and return a list of futures of
-    step objects If run_within_object is False, run thresholds in sequence and return a list of
-    step objects
-    """
-
-    img = image_object.load_step(input_step)
-
-    results = []
-    for thresh in threshold_range:
-        results.append(
-            submit(
-                run_threshold,
-                as_task=run_within_object,
-                image_object=image_object,
-                output_name=output_name,
-                img=img,
-                thresh=thresh,
-                label=label,
-            )
-        )
-    return results
-
-
-@flow(task_runner=create_task_runner(), log_prints=True)
+@flow( log_prints=True)
 def threshold(
     image_object_paths,
+    tags,
     output_name,
     input_step,
     start,
@@ -58,6 +33,7 @@ def threshold(
     step=None,
     n=None,
     label=False,
+    run_type=None,
 ):
     if step is not None:
         # include end in range
@@ -67,33 +43,16 @@ def threshold(
     else:
         raise ValueError("Either `step` or `n` must be provided")
 
-    # if only one image is passed, run across objects within that image. Otherwise, run across images
     image_objects = [ImageObject.parse_file(path) for path in image_object_paths]
-    run_within_object = len(image_objects) == 1
 
-    all_results = []
-    for obj in image_objects:
-        all_results.append(
-            submit(
-                run_object,
-                as_task=not run_within_object,
-                image_object=obj,
-                output_name=output_name,
-                input_step=input_step,
-                threshold_range=threshold_range,
-                run_within_object=run_within_object,
-                label=label,
-            )
+    # TODO this loads each image per threshold instead of loading once and applying all thresholds
+    for thresh in threshold_range:
+        parallelize_across_images(
+            image_objects,
+            run_threshold,
+            tags,
+            output_name=output_name,
+            input_step=input_step,
+            thresh=thresh,
+            label=label,
         )
-
-    for object_result, obj in zip(all_results, image_objects):
-        if run_within_object:
-            # parallelizing within fov
-            object_result = [r.result() for r in object_result]
-        else:
-            # parallelizing across fovs
-            object_result = object_result.result()
-
-        for output in object_result:
-            obj.add_step_output(output)
-        obj.save()
