2468c827ebab588a18836add4b8077be59fef944
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-02-02 11:14:47 -0800

    allow adding tags

--- morflowgenesis/bin/run_step.py
+++ morflowgenesis/bin/run_step.py
@@ -1,12 +1,13 @@
 import asyncio
 
 from hydra._internal.utils import _locate
+from hydra.utils import instantiate
 from prefect import flow, task
 from prefect.deployments import run_deployment
 from prefect.task_runners import ConcurrentTaskRunner
 from slugify import slugify
 
-from morflowgenesis.utils import ImageObject
+from morflowgenesis.utils import ImageObject, run_flow
 
 
 @task()
@@ -20,9 +21,9 @@ def _is_run(path, step_name, output_name):
 
 
 @flow(task_runner=ConcurrentTaskRunner, log_prints=True)
-def get_objects_to_run(working_dir, step_name, output_name):
+def get_objects_to_run(object_store_path, step_name, output_name):
     objects_to_run = []
-    for object_path in working_dir.glob("*.json"):
+    for object_path in object_store_path.glob("*.json"):
         objects_to_run.append(_is_run.submit(object_path, step_name, output_name))
     objects_to_run = [obj.result() for obj in objects_to_run]
     objects_to_run = [obj for obj in objects_to_run if obj is not None]
@@ -62,23 +63,20 @@ async def run_step(step_cfg, object_store_path):
 def run_step_local(step_cfg, object_store_path):
     step_fn = step_cfg["function"]
     step_type = step_cfg.get("step_type", "gather")
+    tags = step_cfg.get("tags", [])
+    task_runner = step_cfg.get("task_runner")
+    task_runner = instantiate(task_runner) if task_runner is not None else ConcurrentTaskRunner()
+    
     step_args = step_cfg["args"]
     step_name = step_fn.split(".")[-1]
     step_fn = _locate(step_fn)
 
-    if step_type == "init":
-        step_fn(**step_args)
-    else:
-        # checking which objects to run here prevents overhead on the cluster and excess job creation.
-        objects_to_run = get_objects_to_run(
-            object_store_path, step_name, step_args.get("output_name")
-        )
-        if len(objects_to_run) == 0:
-            return
-        if step_type == "gather":
-            step_args.update({"image_object_paths": objects_to_run})
-            step_fn(**step_args)
-        else:
-            for object_path in objects_to_run:
-                step_args.update({"image_object_paths": [object_path]})
-                step_fn(**step_args)
+    # checking which objects to run here prevents overhead on the cluster and excess job creation.
+    objects_to_run = get_objects_to_run(
+        object_store_path, step_name, step_args.get("output_name")
+    )
+    if len(objects_to_run) == 0 and object_store_path.exists():
+        return
+    step_args.update({"image_object_paths": objects_to_run})
+    run_type = "images" if step_type == "gather" else "objects" 
+    run_flow(step_fn, task_runner, run_type, tags, **step_args)
