1e1bbdce127fe14d890d574183101d158d99642a
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-02-02 12:19:06 -0800

    remove kubernetes-dependent code

--- morflowgenesis/bin/run_step.py
+++ morflowgenesis/bin/run_step.py
@@ -1,11 +1,7 @@
-import asyncio
-
 from hydra._internal.utils import _locate
 from hydra.utils import instantiate
 from prefect import flow, task
-from prefect.deployments import run_deployment
 from prefect.task_runners import ConcurrentTaskRunner
-from slugify import slugify
 
 from morflowgenesis.utils import ImageObject, run_flow
 
@@ -29,38 +25,7 @@ def get_objects_to_run(object_store_path, step_name, output_name):
     objects_to_run = [obj for obj in objects_to_run if obj is not None]
     return objects_to_run
 
-
-async def run_step(step_cfg, object_store_path):
-    step_fn = step_cfg["function"]
-    step_type = step_cfg.get("step_type", "gather")
-    step_args = step_cfg["args"]
-    step_name = step_fn.split(".")[-1]
-
-    flow_name = slugify(step_name)
-    deployment_name = slugify(step_cfg.get("deployment_name", "default"))
-    full_deployment_name = f"{flow_name}/{deployment_name}"
-    if step_type == "gather":
-        out = await run_deployment(
-            full_deployment_name,
-            parameters=step_args,
-        )
-        return [out]
-    else:
-        # checking which objects to run here prevents overhead on the cluster and excess job creation.
-        objects_to_run = get_objects_to_run(object_store_path, step_name, step_args["output_name"])
-        out = await asyncio.gather(
-            *[
-                run_deployment(
-                    full_deployment_name,
-                    parameters={"image_object_path": object_path, **step_args},
-                )
-                for object_path in objects_to_run
-            ]
-        )
-        return out
-
-
-def run_step_local(step_cfg, object_store_path):
+def run_step(step_cfg, object_store_path):
     step_fn = step_cfg["function"]
     step_type = step_cfg.get("step_type", "gather")
     tags = step_cfg.get("tags", [])
