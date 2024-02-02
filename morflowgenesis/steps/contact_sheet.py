ffdaabd0aac6c9c7c9dfd76e88265c20636d83f0
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-02-02 12:59:03 -0800

    update contact sheet

--- morflowgenesis/steps/contact_sheet.py
+++ morflowgenesis/steps/contact_sheet.py
@@ -3,15 +3,17 @@ import numpy as np
 import pandas as pd
 from aicsimageio import AICSImage
 from omegaconf import ListConfig
-from prefect import flow, task
 from scipy.ndimage import find_objects
 from skimage.exposure import rescale_intensity
 from skimage.segmentation import find_boundaries
 
-from morflowgenesis.utils import ImageObject, StepOutput, create_task_runner, submit
+from morflowgenesis.utils import ImageObject, StepOutput, parallelize_across_images
 
 
-def make_rgb(img, contour):  # this function returns an RGB image
+def make_rgb(img, contour):  
+    """
+    Creates RGB overlay of image and up to 3 contours in C, M, Y
+    """
     img = np.clip(img, np.percentile(img, 0.1), np.percentile(img, 99.9))
     img = rescale_intensity(img, out_range=(0, 255)).astype(np.uint8)
     rgb = np.stack([img] * 3, axis=-1).astype(float)
@@ -62,7 +64,6 @@ def pad_coords(s, padding, constraints):
     return tuple(new_slice)
 
 
-@task
 def project_cell(row, raw_name, seg_names):
     raw = AICSImage(row["crop_raw_path"].iloc[0])
     raw = raw.get_image_dask_data("ZYX", C=raw.channel_names.index(raw_name)).compute()
@@ -77,25 +78,6 @@ def project_cell(row, raw_name, seg_names):
 
     projection = project(raw, seg)
     return projection, row["CellId"].iloc[0]
-
-
-def project_fov(image_object, raw_name, seg_step):
-    raw = image_object.load_step(raw_name)
-    seg = image_object.load_step(seg_step)
-    min_im_shape = np.min([raw.shape, seg.shape], axis=0)
-    raw = raw[: min_im_shape[0], : min_im_shape[1], : min_im_shape[2]]
-    seg = seg[: min_im_shape[0], : min_im_shape[1], : min_im_shape[2]]
-    regions = find_objects(seg)
-    cells = []
-    for val, coords in enumerate(regions, start=1):
-        if coords is None:
-            continue
-        coords = pad_coords(coords, 10, raw.shape)
-        raw_crop = raw[coords]
-        seg_crop = find_boundaries(seg[coords] == val, mode="inner")[None]
-        cells.append((project(raw_crop, seg_crop), val))
-    return cells
-
 
 def assemble_contact_sheet(results, x_bins, y_bins, x_feature, y_feature, title="Contact Sheet"):
     fig, ax = plt.subplots(len(x_bins), len(y_bins), figsize=(4 * len(x_bins), 4 * len(y_bins)))
@@ -115,9 +97,67 @@ def assemble_contact_sheet(results, x_bins, y_bins, x_feature, y_feature, title=
     return fig
 
 
-@flow(task_runner=create_task_runner(), log_prints=True)
+def find_cells_to_plot(n_bins, feature_df, x_feature, y_feature, cell_df, raw_name, seg_names=None):
+    quantile_boundaries = [i / n_bins for i in range(n_bins + 1)]
+    # Use qcut to bin the DataFrame by percentiles across both features
+    x_binned = pd.qcut(feature_df[x_feature], q=quantile_boundaries, duplicates="drop").unique()
+    y_binned = pd.qcut(feature_df[y_feature], q=quantile_boundaries, duplicates="drop").unique()
+    cells = [] 
+    for x_bin in x_binned:
+        for y_bin in y_binned:
+            bin = np.logical_and(
+                x_binned == x_bin,
+                x_binned == y_bin,
+            )
+            if len(bin) > 0:
+                cell_id = np.random.choice(bin.index.get_level_values("CellId").values)
+                cell = cell_df[cell_df["CellId"] == cell_id]
+                cells.append(cell)
+            else:
+                cells.append(None)
+    return cells, x_binned, y_binned
+
+
+
+def generate_fov_contact_sheet(image_object, output_name, raw_name, seg_step):
+    raw = image_object.load_step(raw_name)
+    seg = image_object.load_step(seg_step)
+    min_im_shape = np.min([raw.shape, seg.shape], axis=0)
+    raw = raw[: min_im_shape[0], : min_im_shape[1], : min_im_shape[2]]
+    seg = seg[: min_im_shape[0], : min_im_shape[1], : min_im_shape[2]]
+    regions = find_objects(seg)
+    cells = []
+    for val, coords in enumerate(regions, start=1):
+        if coords is None:
+            continue
+        coords = pad_coords(coords, 10, raw.shape)
+        raw_crop = raw[coords]
+        seg_crop = find_boundaries(seg[coords] == val, mode="inner")[None]
+        cells.append((project(raw_crop, seg_crop), val))
+
+    colors = ["Cyan", "Magenta", "Yellow"]
+
+    title = "Contact Sheet: " + ", ".join(
+        [f"{col}: {name}" for col, name in zip(colors, seg_step)]
+    )
+    n_bins = int(np.ceil(np.sqrt(len(cells))))
+    contact_sheet = assemble_contact_sheet(
+        cells, range(n_bins), range(n_bins), "", "", title=title
+    )
+    output = StepOutput(
+        image_object.working_dir,
+        "segmentation_contact_sheet",
+        output_name,
+        output_type="image",
+        image_id=image_object.id,
+    )
+    contact_sheet.savefig(output.path, dpi=300)
+    return output
+
+
 def segmentation_contact_sheet(
     image_object_paths,
+    tags,
     output_name,
     single_cell_dataset_step,
     feature_step,
@@ -127,45 +167,32 @@ def segmentation_contact_sheet(
     raw_name,
     n_bins=10,
     seg_names=None,
+    run_type =None,
 ):
     if isinstance(seg_names, (list, ListConfig)) and len(seg_names) > 3:
         raise ValueError("Only three segmentation names can be used to create a contact sheet")
+    # load and subset data
     image_objects = [ImageObject.parse_file(path) for path in image_object_paths]
-
     cell_df = pd.concat(
         [image_object.load_step(single_cell_dataset_step) for image_object in image_objects]
     )
     feature_df = pd.concat(
         [image_object.load_step(feature_step) for image_object in image_objects]
     )
-
     feature_df = feature_df.xs(segmentation_name, level="Name")
 
-    quantile_boundaries = [i / n_bins for i in range(n_bins + 1)]
+    # project random cells from each quantile bin
+    plotting_cells, x_binned, y_binned= find_cells_to_plot(n_bins, feature_df, x_feature, y_feature, cell_df, raw_name, seg_names)
+    _, results = parallelize_across_images(plotting_cells, project_cell, tags, data_name='row', create_output = False, raw_name=raw_name, seg_names=seg_names)
+    results = [r if r is not None else (None, None) for r in results]
 
-    # Use qcut to bin the DataFrame by percentiles across both features
-    x_binned = pd.qcut(feature_df[x_feature], q=quantile_boundaries, duplicates="drop")
-    y_binned = pd.qcut(feature_df[y_feature], q=quantile_boundaries, duplicates="drop")
-    results = []
-    for x_bin in x_binned.unique():
-        for y_bin in y_binned.unique():
-            bin = np.logical_and(
-                x_binned == x_bin,
-                x_binned == y_bin,
-            )
-            if len(bin) > 0:
-                cell_id = np.random.choice(bin.index.get_level_values("CellId").values)
-                cell_features = cell_df[cell_df["CellId"] == cell_id]
-                results.append(project_cell.submit(cell_features, raw_name, seg_names))
-            else:
-                results.append(None)
-    results = [r.result() if r is not None else (None, None) for r in results]
+    # assemble contact sheet
     colors = ["Cyan", "Magenta", "Yellow"]
     title = "Contact Sheet: " + ", ".join(
         [f"{col}: {name}" for col, name in zip(colors, seg_names)]
     )
     contact_sheet = assemble_contact_sheet(
-        results, x_binned.unique(), y_binned.unique(), x_feature, y_feature, title=title
+        results, x_binned, y_binned, x_feature, y_feature, title=title
     )
 
     output = StepOutput(
@@ -181,34 +208,13 @@ def segmentation_contact_sheet(
         image_object.save()
 
 
-@flow(task_runner=create_task_runner(), log_prints=True)
 def segmentation_contact_sheet_all(
     image_object_paths,
     output_name,
     raw_name,
     seg_step,
+    tags,
+    run_type
 ):
     image_objects = [ImageObject.parse_file(path) for path in image_object_paths]
-
-    colors = ["Cyan", "Magenta", "Yellow"]
-
-    for image_object in image_objects:
-        cells = project_fov(image_object, raw_name, seg_step)
-        title = "Contact Sheet: " + ", ".join(
-            [f"{col}: {name}" for col, name in zip(colors, seg_step)]
-        )
-        n_bins = int(np.ceil(np.sqrt(len(cells))))
-        contact_sheet = assemble_contact_sheet(
-            cells, range(n_bins), range(n_bins), "", "", title=title
-        )
-        output = StepOutput(
-            image_objects[0].working_dir,
-            "segmentation_contact_sheet",
-            output_name,
-            output_type="image",
-            image_id=image_object.id,
-        )
-        contact_sheet.savefig(output.path, dpi=300)
-        for image_object in image_objects:
-            image_object.add_step_output(output)
-            image_object.save()
+    parallelize_across_images(image_objects, generate_fov_contact_sheet, tags, data_name='image_object', output_name=output_name, raw_name=raw_name, seg_step=seg_step)
