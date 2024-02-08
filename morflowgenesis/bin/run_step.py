06d0fe98d31aca42db69ac5f99e78d4153d4725a
Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>
2024-02-08 13:33:31 -0800

    update task limit setting

--- morflowgenesis/bin/run_step.py
+++ morflowgenesis/bin/run_step.py
@@ -1,33 +1,65 @@
 from hydra._internal.utils import _locate
 from hydra.utils import instantiate
-from prefect import flow, task
+from prefect import flow
+from prefect.client.orchestration import get_client
+from prefect.exceptions import ObjectNotFound
+
+
+import subprocess 
+from datetime import datetime
 from prefect.task_runners import ConcurrentTaskRunner
 
 from morflowgenesis.utils import ImageObject, run_flow
 from prefect.server.schemas.states import StateType
 
 
-@task()
 def _is_run(path, step_name, output_name):
     image_object = ImageObject.parse_file(path)
     # check if step already run
     if image_object.step_is_run(f"{step_name}/{output_name}"):
-        print(f"Skipping step {step_name}/{output_name} for image {image_object.id}")
+        print(f"Skipping {step_name}/{output_name} for  image {image_object.id}")
         return
     return image_object
 
-
-@flow(task_runner=ConcurrentTaskRunner, log_prints=True)
+@flow(log_prints=True)
 def get_objects_to_run(object_store_path, step_name, output_name):
-    objects_to_run = []
-    for object_path in object_store_path.glob("*.json"):
-        objects_to_run.append(_is_run.submit(object_path, step_name, output_name))
-    objects_to_run = [obj.result() for obj in objects_to_run]
+    objects_to_run = [_is_run(object_path, step_name, output_name) for object_path in object_store_path.glob("*.json")]
     objects_to_run = [obj for obj in objects_to_run if obj is not None]
+    print(f'Running {len(objects_to_run)} objects for {step_name}/{output_name}')
     return objects_to_run
 
 
-def run_step(step_cfg, object_store_path):
+def check_state(state, step_cfg):
+    if state != StateType.COMPLETED:
+        raise RuntimeError(f"Step {step_cfg['function']} completed with state {state}")
+
+async def setup_task_limits(step_cfg):
+    """Set up task limits for the step"""
+    task_limit = step_cfg.get('task_runner', {}).get('task_limit')
+    if task_limit:
+        # create unique tag based on task function and submission time
+        task_name = f'{step_cfg["function"]}_{datetime.now().strftime("%Y%m%d%H%M%S")}'
+        # set task limits
+        async with get_client() as client:
+            await client.create_concurrency_limit(tag=task_name, concurrency_limit=task_limit)
+        print(f'Set task limit for {step_cfg["function"]} to {task_limit}')
+        del step_cfg['task_runner']['task_limit']
+    return step_cfg
+
+async def tear_down_task_limits(step_cfg):
+    if 'tags' not in step_cfg:
+        return
+    
+    async with get_client() as client:
+        try:
+            await client.delete_concurrency_limit_by_tag(tag=step_cfg['tags'][0])
+        except ObjectNotFound:
+            print('Concurrency limit not found')
+
+
+async def run_step(step_cfg, object_store_path):
+    step_cfg = await setup_task_limits(step_cfg)
+
     step_fn = step_cfg["function"]
     tags = step_cfg.get("tags", [])
     task_runner = step_cfg.get("task_runner")
@@ -42,4 +74,8 @@ def run_step(step_cfg, object_store_path):
     if len(objects_to_run) == 0 and object_store_path.exists():
         return StateType.COMPLETED
     step_args.update({"image_objects": objects_to_run})
-    return run_flow(step_fn, task_runner, tags, **step_args)
+    result = run_flow(step_fn, task_runner, tags, **step_args)
+    await tear_down_task_limits(step_cfg)
+    check_state(result, step_cfg)
+
+
--- morflowgenesis/bin/run_workflow.py
+++ morflowgenesis/bin/run_workflow.py
@@ -1,13 +1,10 @@
 import asyncio
 import logging
 from pathlib import Path
-import subprocess 
-from datetime import datetime
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from prefect import flow
 from prefect.task_runners import SequentialTaskRunner
-from prefect.server.schemas.states import StateType
 
 from morflowgenesis.bin.run_step import run_step
 
@@ -19,31 +16,6 @@ def save_workflow_config(working_dir, cfg):
     with open(Path(working_dir) / "workflow_config.yaml", "w") as f:
         OmegaConf.save(config=cfg, f=f)
 
-def check_state(state, step_cfg):
-    if state != StateType.COMPLETED:
-        raise RuntimeError(f"Step {step_cfg['function']} completed with state {state}")
-
-def setup_task_limits(step_cfg):
-    """Set up task limits for the step"""
-    task_limit = step_cfg.get('task_runner', {}).get('task_limit')
-    if task_limit:
-        # create unique tag based on task function and submission time
-        task_name = f'{step_cfg["function"]}_{datetime.now().strftime("%Y%m%d%H%M%S")}'
-        # set task limits
-        command = ["prefect", "concurrency-limit", "create", task_name, str(task_limit)]
-        # Run the command
-        subprocess.run(command, check=True)
-        step_cfg['tags'] = [task_name]
-        del step_cfg['task_runner']['task_limit']
-    return step_cfg
-
-def tear_down_task_limits(step_cfg):
-    if 'tags' not in step_cfg:
-        return
-    command = ["prefect", "concurrency-limit", "delete", step_cfg['tags'][0]]
-    # Run the command
-    subprocess.run(command, check=True)
-
 
 @flow(log_prints=True, task_runner=SequentialTaskRunner())
 async def morflowgenesis(cfg):
@@ -54,10 +26,7 @@ async def morflowgenesis(cfg):
     save_workflow_config(working_dir, cfg)
 
     for step_cfg in cfg["steps"]:
-        step_cfg = setup_task_limits(step_cfg)
-        result = run_step(step_cfg, working_dir / "_ImageObjectStore")
-        tear_down_task_limits(step_cfg)
-        check_state(result, step_cfg)
+        await run_step(step_cfg, working_dir / "_ImageObjectStore")
 
 # default config is morflowgenesis/configs/workflow_config.yaml
 @hydra.main(version_base="1.3", config_path="../configs/", config_name="workflow_config.yaml")
