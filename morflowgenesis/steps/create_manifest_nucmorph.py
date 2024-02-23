44d4c7bac1f47879e7bf0cf3051532363d6faac6
Benjamin Morris <benjamin.morris@prd-aics-dcp-039.corp.alleninstitute.org>
2024-02-23 10:37:47 -0800

    make column from each source explicit

--- morflowgenesis/steps/create_manifest_nucmorph.py
+++ morflowgenesis/steps/create_manifest_nucmorph.py
@@ -23,27 +23,27 @@ def create_manifest(
     manifest = []
     for obj in tqdm.tqdm(image_objects, desc="Loading single cell features..."):
         features = obj.load_step(feature_step)
-        cells = obj.load_step(single_cell_step)
+        cells = obj.load_step(single_cell_step)[['CellId', 'roi', 'scale_micron', 'centroid_z', 'centroid_y', 'centroid_x', 'label_img', '20x_lamin_path', 'nuc_seg_path', 'crop_raw_id', 'crop_raw_path', 'crop_seg_id', 'crop_seg_path']]
         cells_with_feats = pd.merge(cells, features, on="CellId", how="outer")
-        drop_cols = ["shcoeff", "name_dict", "channel_names_seg"] + [
+        drop_cols = ["shcoeff"] + [
             col for col in cells_with_feats.columns if re.search("Unnamed", col)
         ]
         cells_with_feats = cells_with_feats.drop(columns=drop_cols, errors="ignore")
+        cells_with_feats['index_sequence'] = obj.metadata['T']
         manifest.append(cells_with_feats)
     manifest = pd.concat(manifest)
 
-    if breakdown_classification_step is not None:
+    # load tracking data
+    tracking = obj.load_step(tracking_step)[['index_sequence', 'label_img', 'edge_cell', 'track_id', 'lineage_id']]
+    manifest = pd.merge(manifest, tracking, on=['index_sequence', 'label_img'], how="left")
+    manifest = manifest.rename(columns={'label_img_x': 'label_img', 'edge_cell': 'fov_edge'})
+
+    if breakdown_classification_step in obj.steps:
         # add formation/breakdown information based on track_id
         # same for all objects, just load once
         breakdown_classification = obj.load_step(breakdown_classification_step)
         breakdown_classification = breakdown_classification[['track_id', 'formation', 'breakdown']]
         manifest = pd.merge(manifest, breakdown_classification, on="track_id", how="left")
-
-    # load tracking data
-    tracking = obj.load_step(tracking_step)[['index_sequence', 'label_img', 'edge_cell', 'track_id', 'lineage_id']]
-    manifest = pd.merge(manifest, tracking, on=['index_sequence', 'label_img'], how="left")
-    manifest = manifest.drop(columns=['index_sequence_y', 'label_img_y'])
-    manifest = manifest.rename(columns={'index_sequence_x': 'index_sequence', 'label_img_x': 'label_img', 'edge_cell': 'fov_edge'})
 
     # sort manifest
     manifest = manifest.sort_values(by="index_sequence")
@@ -68,3 +68,4 @@ def create_manifest(
     for obj in image_objects:
         obj.add_step_output(step_output)
         obj.save()
+
