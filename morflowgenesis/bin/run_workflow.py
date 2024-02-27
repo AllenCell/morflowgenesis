dae8ab5e3ed69b3c070342db3790e1029db47076
benjijamorris <54606172+benjijamorris@users.noreply.github.com>
2024-02-27 14:32:10 -0800

    Feature/end summarize (#16)
    * add summary generation after workflow
    
    * run summarize
    
    ---------
    
    Co-authored-by: Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>

--- morflowgenesis/bin/run_workflow.py
+++ morflowgenesis/bin/run_workflow.py
@@ -4,16 +4,45 @@ import os
 from pathlib import Path
 
 import hydra
-import pyrootutils
+import pandas as pd
+import tqdm
 from omegaconf import DictConfig, OmegaConf
 from prefect import flow
 from prefect.task_runners import SequentialTaskRunner
 
 from morflowgenesis.bin.run_step import run_step
+from morflowgenesis.utils import ImageObject
 from morflowgenesis.utils.rich_utils import print_config_tree
 
 # suppress info logging from dask
 logging.getLogger("distributed").setLevel(logging.ERROR)
+
+
+def append(data, k, v):
+    if k not in data:
+        data[k] = []
+    data[k].append(v)
+    return data
+
+
+def generate_summary(working_dir):
+    """Generate a summary of the workflow results."""
+    image_objects = [
+        ImageObject.parse_file(fn)
+        for fn in tqdm.tqdm(
+            (working_dir / "_ImageObjectStore/").glob("*"), desc="Loading image objects"
+        )
+    ]
+    data = {"id": [], "source_path": []}
+
+    for obj in tqdm.tqdm(image_objects, desc="Creating CSV"):
+        data["id"].append(obj.id)
+        data["source_path"].append(obj.source_path)
+        for k, v in obj.metadata.items():
+            append(data, k, v)
+        for step in obj.steps:
+            append(data, step, obj.steps[step].path)
+    pd.DataFrame(data).to_csv(working_dir / "metadata.csv", index=False)
 
 
 @flow(log_prints=True, task_runner=SequentialTaskRunner())
@@ -25,6 +54,7 @@ async def morflowgenesis(cfg):
 
     for step_name, step_cfg in workflow["steps"].items():
         await run_step(step_cfg, working_dir / "_ImageObjectStore")
+    generate_summary(working_dir)
 
 
 def clean_config(cfg):
