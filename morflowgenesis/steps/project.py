37a8e27c81fa4b21bda2674ac9c03062ce956a9f
benjijamorris <54606172+benjijamorris@users.noreply.github.com>
2024-02-27 14:31:32 -0800

    add tracking projection (#15)
    Co-authored-by: Benjamin Morris <benjamin.morris@prd-aics-dcp-037.corp.alleninstitute.org>

--- morflowgenesis/steps/project.py
+++ morflowgenesis/steps/project.py
@@ -24,12 +24,22 @@ def run_project(
     project_slice=None,
     axis=None,
     intensity_rescale_range=None,
+    tracking_step=None,
 ):
+    output = StepOutput(
+        image_object.working_dir,
+        step_name="project",
+        output_name=f"{output_name}_{input_step}_{project_type}_{project_slice}_{axis}",
+        output_type="image",
+        image_id=f"{image_object.metadata['T']}.tif",
+    )
+    if output.path.exists():
+        return output
     # image resizing
     img = image_object.load_step(input_step)
     if project_type == "max":
         img = np.max(img, 0)
-    if project_type == "slice":
+    elif project_type == "slice":
         assert isinstance(project_slice, int), "project_slice must be an integer"
         if axis is None or axis == 0:
             img = img[project_slice]
@@ -39,7 +49,6 @@ def run_project(
             img = img[:, :, project_slice]
     else:
         raise ValueError(f"project_type {project_type} not recognized")
-
     img = rescale(img, scale, order=0, preserve_range=True, anti_aliasing=False)
     if intensity_rescale_range is not None:
         if not isinstance(intensity_rescale_range, str):
@@ -47,14 +56,23 @@ def run_project(
         img = rescale_intensity(img, in_range=intensity_rescale_range, out_range=dtype).astype(
             dtype
         )
+
+    if tracking_step is not None:
+        tracking = image_object.load_step(tracking_step)
+        tracking = tracking[tracking.index_sequence == image_object.metadata["T"]][
+            ["label_img", "track_id"]
+        ]
+        # limit # of track_id values to prevent overflow
+        tracking["track_id"] = tracking["track_id"] % 512
+        offset = tracking["track_id"].max() + 1
+        # make sure we don't overwrite track ids
+        tracking["track_id"] += offset
+        # relabel
+        for row in tracking.itertuples():
+            img[img == row.label_img] = row.track_id
+        img[img > 0] -= offset - 1  # don't map track_id 0 to 0
+
     # add result to image object
-    output = StepOutput(
-        image_object.working_dir,
-        step_name="project",
-        output_name=f"{output_name}_{input_step}_{project_type}_{project_slice}_{axis}",
-        output_type="image",
-        image_id=image_object.id,
-    )
     output.save(img)
     image_object.add_step_output(output)
     image_object.save()
@@ -72,6 +90,7 @@ def project(
     project_slice: int = None,
     axis: int = None,
     intensity_rescale_ranges: List[float] = None,
+    tracking_step: str = None,
 ):
     """
     Axis project images or extract slice from image and rescale intensities
@@ -98,9 +117,12 @@ def project(
     intensity_rescale_ranges : List[float], optional
         List of intensity rescale ranges to use for each input step
     """
-    assert len(intensity_rescale_ranges) == len(
-        input_steps
-    ), "intensity_rescale_ranges must be the same length as input_steps"
+    if intensity_rescale_ranges is None:
+        intensity_rescale_ranges = [None] * len(input_steps)
+    if len(intensity_rescale_ranges) != len(input_steps):
+        raise ValueError(
+            "intensity_rescale_ranges must be the same length as input_steps if provided"
+        )
 
     input_steps = to_list(input_steps)
     dtype = get_class(dtype)
@@ -117,5 +139,6 @@ def project(
             project_type=project_type,
             project_slice=project_slice,
             axis=axis,
-            intensity_rescale_ranges=intensity_rescale_ranges[i],
+            intensity_rescale_range=intensity_rescale_ranges[i],
+            tracking_step=tracking_step,
         )
